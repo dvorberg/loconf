b96eea7c1834ac59d890ce048236b373f985417e
Diedrich Vorberg <diedrich@tux4web.de>
2024-05-06 11:56:01 +0200

    Use common_args.

--- loconf/tools/readcvs.py
+++ loconf/tools/readcvs.py
@@ -1,45 +1,38 @@
 #!/usr/bin/env python
 
+"""
+Read CVs from the locomotive currently on the programming track.
+"""
+
 import sys, argparse, pathlib, re, datetime
+
 from loconf import config, debug
 
-from loconf.language.parser import Parser
+from . import common_args
+from .utils import read_names_file
 
 cv_re = re.compile(r"(\d+)-(\d+)|(\d+)")
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
 
-    parser.add_argument("-d", "--debug", action="store_true",
-                        help="Output debug information to stderr.")
-    parser.add_argument("-dc", "--comdebug", action="store_true",
-                        help="Output communications with the station "
-                        "to stderr.")
-    parser.add_argument("-ds", "--sqldebug", action="store_true",
-                        help="Output SQL commands and queries to stderr.")
+    common_args.debug(parser)
+    common_args.cab(parser, "To make sure you are changing the "
+                    "correct locomotives’s CVs, provide the cab number  "
+                    "of the loco on the programming track here."),
+    common_args.names_file(parser)
+    common_args.revision_comment(parser)
 
-    parser.add_argument("-c", "--cab", type=int, required=True,
-                        help="To make sure you are changing the correct "
-                        "locomotives’s CVs, provide the cab number of the "
-                        "loco on the programming track here.")
     parser.add_argument("-o", "--outfile", type=argparse.FileType('w'),
                         default=sys.stdout, help="Output file. "
                         "Defaults to stdout.")
-    parser.add_argument("-n", "--names-file", type=argparse.FileType('r'),
-                        default=None, help="Read variable name definitions "
-                        "from this file. These names will be used as left "
-                        "hand operands as in “name” := “value”.")
+
     parser.add_argument("ranges", nargs="+",
                         help= "CV numbers may be single numbers, separated by "
                         "comma or space or ranges as in start-stop.")
 
     args = parser.parse_args()
 
-    debug_conf = {}
-    for d in ("debug", "sqldebug", "comdebug"):
-        if getattr(args, d):
-            debug_conf[d] = True
-    config.update(debug_conf)
-
+    common_args.set_debug_config(args)
 
     def find_cvs(s):
         match = cv_re.match(s)
@@ -60,17 +53,15 @@ def main():
                     yield cv
 
     now = datetime.datetime.now()
-    print(f"CVs read from address {args.cab} on {now.ctime()}",
+    print(f"# CVs read from address {args.cab} on {now.ctime()}",
           file=args.outfile)
     print(file=args.outfile)
+
 
     if args.names_file is None:
         names = {}
     else:
-        parser = Parser()
-        parser.parse(args.names_file)
-        names = dict( [(value, name,)
-                       for (name, value) in parser.variables.items()] )
+        names = read_names_file(args.names_file)
 
         print(f'include "{args.names_file.name}"', file=args.outfile)
         print("", file=args.outfile)
@@ -87,18 +78,26 @@ def main():
     else:
         debug("verified!", color="green")
 
-    for cv in cvs:
-        debug("Reading CV", cv, end=" ")
-        value = station.readcv(cv)
+    to_be_stored = {}
+    try:
+        for cv in cvs:
+            debug("Reading CV", cv, end=" ")
+            value = station.readcv(cv)
 
-        if cv in names:
-            left_hand = names[cv]
-        else:
-            left_hand = cv
+            if cv in names:
+                left_hand = names[cv]
+            else:
+                left_hand = cv
 
-        debug(left_hand, ":=", value, color="grey")
+            debug(left_hand, ":=", value, color="grey")
 
-        print(left_hand, ":=", value, file=args.outfile)
+            print(left_hand, ":=", value, file=args.outfile)
+    except StationException:
+        raise
+    finally:
+        # Store the read cvs in the database.
+        config.database.store(cab, to_be_stored, args.revision_comment)
+
 
 if __name__ == "__main__":
     main()
