a4268406b3f83f06399bd66640cab1254394e852
Diedrich Vorberg <diedrich@tux4web.de>
2024-08-28 19:48:05 +0200

    Moved to ..

--- loconf/tools/utils.py
+++
@@ -1,7 +0,0 @@
-from loconf.language.parser import Parser
-
-def read_names_file(fp):
-    parser = Parser()
-    parser.parse(fp)
-    return dict( [(value, name,)
-                  for (name, value) in parser.variables.items()] )
