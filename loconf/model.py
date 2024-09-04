1c53b33eb841d8d8b62ca054981361facbe09b3d
Diedrich Vorberg <diedrich@tux4web.de>
2024-09-05 00:37:54 +0200

    Renamed vehicle.identifyer to nickname and .name to .designation. Much better names.

--- loconf/model.py
+++ loconf/model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from .database.object import dbobject
+from .utils import VehicleIdentifyer
 
 class Revision(dbobject):
     __relation__ = "revision"
@@ -9,9 +10,13 @@ class Vehicle(dbobject):
     __relation__ = "roster"
 
     # The primary key in the database is (address, vehicle_id). My miniorm
-    # can’t do that. The identifyer column is unique, though, and may stand
+    # can’t do that. The nickname column is unique, though, and may stand
     # in for the “real” primary key here.
-    __primary_key_column__ = ( "identifyer", )
+    __primary_key_column__ = ( "nickname", )
+
+    @property
+    def id(self):
+        return VehicleIdentifyer(self.address, self.vehicle_id)
 
     def __repr__(self):
         if self.vehicle_id:
@@ -19,4 +24,7 @@ class Vehicle(dbobject):
         else:
             addr = str(self.address)
 
-        return f"<Vehicle '{self.identifyer}' ({addr})>"
+        return f"<Vehicle '{self.nickname}' ({addr})>"
+
+    def __str__(self):
+        return f"{self.designation or self.nickname} ({self.id})"
